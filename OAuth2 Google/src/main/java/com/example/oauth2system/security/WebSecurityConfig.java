7cc71a25355b6bd4f49c6bde80b60ef24885a272
Sarthak <sarthak.paneru007@gmail.com>
2024-04-23 00:02:12 +0545

    removed unwanted code

--- OAuth2 Google/src/main/java/com/example/oauth2system/security/WebSecurityConfig.java
+++ OAuth2 Google/src/main/java/com/example/oauth2system/security/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.oauth2system.security;
 
-import com.example.oauth2system.config.HttpCookieOAuth2AuthorizationRequestRepository;
+//import com.example.oauth2system.config.HttpCookieOAuth2AuthorizationRequestRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -58,16 +58,6 @@ public class WebSecurityConfig {
 
             return mappedAuthorities;
         };
-    }
-
-    /*
-      By default, Spring OAuth2 uses HttpSessionOAuth2AuthorizationRequestRepository to save
-      the authorization request. But, since our service is stateless, we can't save it in
-      the session. We'll save the request in a Base64 encoded cookie instead.
-    */
-    @Bean
-    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
-        return new HttpCookieOAuth2AuthorizationRequestRepository();
     }
 
     @Bean
